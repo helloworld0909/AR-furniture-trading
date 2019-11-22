1e186da5653c8679eae57dd72fca440e797e548e
luoz14 <luoz14@uci.edu>
2019-11-21 21:58:04 -0800

    Allows swipe gesture to refresh the item list on the buyer fragment
    https://trello.com/c/2a621T2B

--- app/src/main/java/edu/uci/cs297p/arfurniture/buyer/BuyerFragment.java
+++ app/src/main/java/edu/uci/cs297p/arfurniture/buyer/BuyerFragment.java
@@ -1,104 +1,74 @@
 package edu.uci.cs297p.arfurniture.buyer;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import edu.uci.cs297p.arfurniture.R;
 import edu.uci.cs297p.arfurniture.item.Item;
-import edu.uci.cs297p.arfurniture.seller.PostItemFragment;
 
 
 public class BuyerFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_buyer, viewGroup, false);
+        return inflater.inflate(R.layout.fragment_buyer, viewGroup, false);
+    }
 
-        RecyclerView itemRecyclerView = rootView.findViewById(R.id.item_list);
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        SwipeRefreshLayout swipeLayout = view.findViewById(R.id.swipe_container);
+
+        swipeLayout.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light);
+
+        RecyclerView itemRecyclerView = view.findViewById(R.id.item_list);
 
         // use a linear layout manager
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
         itemRecyclerView.setLayoutManager(layoutManager);
 
-        List<Item> dataSet = new ArrayList<>();
+        ItemAdapter itemAdapter = new ItemAdapter(getContext(), new ArrayList<>(),
+                (Item item) -> {
+                    Fragment itemFragment = new ItemFragment();
+                    Bundle args = new Bundle();
+                    args.putSerializable("item", item);
+                    itemFragment.setArguments(args);
 
-        RecyclerView.Adapter adapter = new ItemAdapter(getContext(), dataSet, (Item item) ->
-        {
-            Fragment itemFragment = new ItemFragment();
-            Bundle args = new Bundle();
-            args.putSerializable("item", item);
-            itemFragment.setArguments(args);
+                    getFragmentManager().beginTransaction()
+                            .setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out)
+                            .add(R.id.content_container, itemFragment)
+                            .addToBackStack(null)
+                            .commit();
+                },
 
-            getFragmentManager().beginTransaction()
-                    .setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out)
-                    .add(R.id.content_container, itemFragment)
-                    .addToBackStack(null)
-                    .commit();
-        }
-        );
-        itemRecyclerView.setAdapter(adapter);
+                new OnRefreshEventListener() {
+                    @Override
+                    public void onRefreshStart() {
+                        swipeLayout.setRefreshing(true);
+                    }
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        // Create a reference to the cities collection
-        CollectionReference itemsRef = db.collection("items");
-
-        // Create a query against the collection.
-        Task<QuerySnapshot> query = itemsRef.get();
-        query.addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                for (QueryDocumentSnapshot document : task.getResult()) {
-                    Log.d("TEST", document.getId() + " => " + document.getData());
-                    dataSet.add(constructItem(document));
+                    @Override
+                    public void onRefreshEnd() {
+                        swipeLayout.setRefreshing(false);
+                    }
                 }
-                adapter.notifyDataSetChanged();
+        );
 
-            } else {
-                Log.d("TEST", "Error getting documents: ", task.getException());
-            }
-        });
-
-        return rootView;
-    }
-
-    private Item constructItem(QueryDocumentSnapshot document) {
-        Item item = new Item(document.getId(), document.get("name").toString(),
-                document.get("description").toString(), document.get("price").toString(), Integer.valueOf(document.get("category").toString()));
-        if (document.contains("imageURLs")) {
-            item.setImageUrls((List<String>) document.get("imageURLs"));
-        }
-        if (document.contains("modelName")) {
-            item.setModelName(document.get("modelName").toString());
-        }
-
-        if (document.contains(PostItemFragment.SCALE_KEY)) {
-            final List<Double> scale = (List<Double>) document.get(PostItemFragment.SCALE_KEY);
-            if (scale != null && scale.size() == 3) {
-                item.setScale(scale);
-            } else {
-                throw new IllegalArgumentException("Scale data is wrong");
-            }
-        }
-
-        if (document.contains(PostItemFragment.COLOR_KEY)) {
-            final long color = (long) document.get(PostItemFragment.COLOR_KEY);
-            item.setColor((int) color);
-        }
-
-        return item;
+        swipeLayout.setOnRefreshListener(itemAdapter);
+        itemRecyclerView.setAdapter(itemAdapter);
+        itemAdapter.refresh();
     }
 }
--- app/src/main/java/edu/uci/cs297p/arfurniture/buyer/ItemAdapter.java
+++ app/src/main/java/edu/uci/cs297p/arfurniture/buyer/ItemAdapter.java
@@ -3,6 +3,7 @@ package edu.uci.cs297p.arfurniture.buyer;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,19 +13,28 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.List;
 
 import edu.uci.cs297p.arfurniture.ARActivity;
 import edu.uci.cs297p.arfurniture.R;
 import edu.uci.cs297p.arfurniture.item.Item;
+import edu.uci.cs297p.arfurniture.seller.PostItemFragment;
 
 
-public class ItemAdapter extends RecyclerView.Adapter {
+public class ItemAdapter extends RecyclerView.Adapter implements SwipeRefreshLayout.OnRefreshListener {
 
     private final Context mContext;
     private List<Item> mDataSet;
     private OnItemClickListener mListener;
+    private OnRefreshEventListener mRefreshedListener;
 
     public static class ItemViewHolder extends RecyclerView.ViewHolder {
         public ItemViewHolder(CardView view) {
@@ -40,10 +50,11 @@ public class ItemAdapter extends RecyclerView.Adapter {
         }
     }
 
-    public ItemAdapter(Context context, List<Item> data, OnItemClickListener listener) {
+    public ItemAdapter(Context context, List<Item> data, OnItemClickListener listener, OnRefreshEventListener refreshedListener) {
         mContext = context;
         mDataSet = data;
         mListener = listener;
+        mRefreshedListener = refreshedListener;
     }
 
     @NonNull
@@ -86,8 +97,73 @@ public class ItemAdapter extends RecyclerView.Adapter {
         intent.putExtras(params);
         return intent;
     }
+
+    public void refresh() {
+        Log.d("ItemAdapter", "refresh");
+
+        mRefreshedListener.onRefreshStart();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        // Create a reference to the cities collection
+        CollectionReference itemsRef = db.collection("items");
+
+        mDataSet.clear();
+
+        // Create a query against the collection.
+        Task<QuerySnapshot> query = itemsRef.get();
+        query.addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                for (QueryDocumentSnapshot document : task.getResult()) {
+                    Log.d(getClass().getName(), document.getId() + " => " + document.getData());
+                    mDataSet.add(constructItem(document));
+                }
+                notifyDataSetChanged();
+                mRefreshedListener.onRefreshEnd();
+            } else {
+                Log.e(getClass().getName(), "Error getting documents: ", task.getException());
+                mRefreshedListener.onRefreshEnd();
+            }
+        });
+    }
+
+    private Item constructItem(QueryDocumentSnapshot document) {
+        Item item = new Item(document.getId(), document.get("name").toString(),
+                document.get("description").toString(), document.get("price").toString(), Integer.valueOf(document.get("category").toString()));
+        if (document.contains("imageURLs")) {
+            item.setImageUrls((List<String>) document.get("imageURLs"));
+        }
+        if (document.contains("modelName")) {
+            item.setModelName(document.get("modelName").toString());
+        }
+
+        if (document.contains(PostItemFragment.SCALE_KEY)) {
+            final List<Double> scale = (List<Double>) document.get(PostItemFragment.SCALE_KEY);
+            if (scale != null && scale.size() == 3) {
+                item.setScale(scale);
+            } else {
+                throw new IllegalArgumentException("Scale data is wrong");
+            }
+        }
+
+        if (document.contains(PostItemFragment.COLOR_KEY)) {
+            final long color = (long) document.get(PostItemFragment.COLOR_KEY);
+            item.setColor((int) color);
+        }
+
+        return item;
+    }
+
+    @Override
+    public void onRefresh() {
+        refresh();
+    }
 }
 
 interface OnItemClickListener {
     void onItemClick(Item item);
+}
+
+interface OnRefreshEventListener {
+    void onRefreshStart();
+
+    void onRefreshEnd();
 }
